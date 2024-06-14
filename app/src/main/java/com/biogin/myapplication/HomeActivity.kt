fec7bc25c0a2e436fcafc26dad013afd1cd0bcb7
Mariano Chun <marianochun01@gmail.com>
2024-06-13 22:48:19 -0300

    Add configuración para tener siempre activo el light mode

--- app/src/main/java/com/biogin/myapplication/HomeActivity.kt
+++ app/src/main/java/com/biogin/myapplication/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.biogin.myapplication.data.LogsRepository
@@ -21,6 +22,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         MasterUserDataSession.clearUserData()
         viewBinding = ActivityHomeBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(viewBinding.root)
         enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
